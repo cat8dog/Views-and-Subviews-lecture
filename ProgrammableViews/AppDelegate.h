4e6efdfbb829026f35555a2de2f4a3a1ad445481
cat8dog <catherine.reyto@gmail.com>
2015-04-15 10:15:08 -0400

    Initial Commit

---
+++ ProgrammableViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgrammableViews
+//
+//  Created by Catherine Reyto on 2015-04-15.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
